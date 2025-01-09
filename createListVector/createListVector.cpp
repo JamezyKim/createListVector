6477b99fdca55aecf8ad6438d6e92d9f519e9d9f
James Kim <yt4921@gmail.com>
2025-01-09 12:13:39 -0800

    fix the main and create the function pushFront

--- createListVector/createListVector.cpp
+++ createListVector/createListVector.cpp
@@ -1,51 +1,70 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Node {
+private:
 
-	class Node {
-	private:
+public:
+	int value;
+	Node* next;
+	Node* prev;
+	Node() {};
+	Node(int value) {
+		this->value = value;
+		this->next = NULL;
+		this->prev = NULL;
+	}
 
-	public:
-		int value;
-		Node* next;
-		Node* prev;
-		Node() {};
-		Node(int value) {
-			this->value = value;
-			this->next = NULL;
-			this->prev = NULL;
+	~Node() {}
+};
+
+class LinkedList {
+private:
+	Node* front;
+	Node* back;
+	int size;
+public:
+
+	LinkedList() {
+		this->front = NULL;
+		this->back = NULL;
+		this->size = 0;
+	}
+
+	void printNodes() {
+		while (front->next != NULL) {
+			cout << front->value << " -> ";
+			front = front->next;
 		}
-
-		~Node() {}
-	};
-
-	class LinkedList {
-	private:
-		Node* front;
-		Node* back;
-		int size;
-	public:
-
-		LinkedList() {
-			this->front = NULL;
-			this->back = NULL;
-			this->size = 0;
+		if (front->next == NULL) {
+			cout << front->value;
 		}
+		return;
+	}
 
-		void printNodes() {
-			while (front->next != NULL) {
-				cout << front->value << " -> ";
-				front = front->next;
-			}
-			if (front->next == NULL) {
-				cout << front->value;
-			}
+	void pushFront(int value) {
+		Node* newNode = new Node(value);
+		if (this->front == NULL && this->back == NULL) {
+			this->front = newNode;
+			this->back = newNode;
+			size++;
 			return;
 		}
+		newNode->next = front;
+		front = newNode;
+		newNode->value = value;
+		newNode->next->prev = newNode;
+		size++;
+		return;
+	}
+	~LinkedList() {}
+};
+int main() {
 
-		~LinkedList() {}
-	};
+
+
+
+
 
 	return 0;
 }
